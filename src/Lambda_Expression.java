0cb2f34a725bcf3f0e4959a1e44a53f39613d855
adrenalinejunkie03 <muhammadsuhaib2805@gmail.com>
2026-03-18 10:07:38 +0500

    Interface, Functional interface and more info

--- src/Lambda_Expression.java
+++ src/Lambda_Expression.java
@@ -5,7 +5,29 @@ public class Lambda_Expression {
         // Before Java 8
         Thread thread = new Thread(new Task());
 
+        // All this code can be replaced with a lambda expression just like this: () -> {}
+        // Your code goes into curly braces and round braces are for parameters if needed. In this case we don't have any parameters so we just leave it empty.
+        // After Java 8
+        Thread thread1 = new Thread(() -> {
+            System.out.println("Task is running...");
+        });
+
+        // It is used to implement functional interfaces.
+        // A functional interface is an interface in which there is only one abstract method.
+        // An abstract method is a method that is declared without an implementation. It is meant to be overridden by subclasses.
+        // Another example:
+
+        MathOperation addOperation = (a, b) -> a + b;
+        // It is a one-liner and there's no need for curly braces or return statement because the expression is simple and returns a value directly.
+        MathOperation multiplyOperation = (a, b) -> a * b;
+        System.out.println("Addition: " + addOperation.operate(5, 3)); // Output: 8
+        System.out.println("Multiplication: " + multiplyOperation.operate(5, 3));
+
     }
+}
+
+interface  MathOperation {
+    int operate(int a, int b);
 }
 
 class Task implements Runnable{
