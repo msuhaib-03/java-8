cacaadf72a96c4798cf6af1522020064f23f5010
adrenalinejunkie03 <muhammadsuhaib2805@gmail.com>
2026-03-18 22:39:07 +0500

    Supplier with only get() and consumer with only 1 arg; no return

---
+++ src/Supplier_Demo.java
@@ -0,0 +1,11 @@
+import java.util.function.Supplier;
+
+public class Supplier_Demo {
+    public static void main(String[] args) {
+        // It has only 1 method: get() and no default & no static methods. It is used to generate or supply values without taking any input. It is a functional interface that takes no arguments and returns a value of type T.
+        // We can use lambda expressions to create instances of Supplier interface.
+
+        Supplier<String> stringSupplier = () -> "Hey! I'm Supplier.";
+        System.out.println(stringSupplier.get());
+    }
+}
