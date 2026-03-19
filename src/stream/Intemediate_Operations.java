44591298220e9b99d54d799b69b5bb8804259961
adrenalinejunkie03 <muhammadsuhaib2805@gmail.com>
2026-03-19 08:42:02 +0500

    Intermediate Operations in Stream with description and example code

---
+++ src/stream/Intemediate_Operations.java
@@ -0,0 +1,43 @@
+package stream;
+
+import java.util.List;
+import java.util.stream.Stream;
+
+public class Intemediate_Operations {
+    public static void main(String[] args) {
+        // Transform a stream into another stream
+        // They are lazy, meaning they won't execute until a terminal operation is involved.
+        // Filter takes predicate which holds a condition & causes intermediate operations.
+
+        // 1) Filter: It takes a predicate and returns a stream that contains only the elements that match the predicate.
+        List<String> names = List.of("Amie", "Amjad", "Rabz");
+        Stream<String> a = names.stream().filter(x -> x.startsWith("A"));
+        System.out.println("Filter Stream: ");
+        // Now this stream won't work until we associate terminal operation with it like .count() or .forEach() etc.
+        System.out.println(a.count()); // Output: 2
+
+        // 2) Map: It takes a function and returns a stream that contains the results of applying the function to the elements of the original stream.
+        List<Integer> numbers = List.of(4,5,1,3,2);
+        numbers.stream().map(x -> x*x).filter(x -> x%2==0).forEach(System.out::println);
+        System.out.println("Map Stream: ");
+        System.out.println(numbers);
+
+        // 3) Sorted: It returns a stream that contains the elements of the original stream sorted according to natural order or a specified comparator.
+        Stream<Integer> sorted = numbers.stream().sorted();
+        System.out.println("Sorted Stream: ");
+        sorted.forEach(System.out::println);
+
+        // 4) Distinct: It returns a stream that contains the distinct elements of the original stream.
+        long count = numbers.stream().filter(x -> x % 3 == 0).distinct().count();
+        System.out.println("Distinct Stream: ");
+        System.out.println(count); // Output: 1
+
+        // 5) Limit: It returns a stream that contains the first n elements of the original stream.
+        System.out.println("Limit Stream: ");
+        numbers.stream().limit(3).forEach(System.out::println);
+
+        // 6) Skip: It returns a stream that contains the elements of the original stream except the first n elements.
+        System.out.println("Skip Stream: ");
+        numbers.stream().skip(3).forEach(System.out::println);
+    }
+}
